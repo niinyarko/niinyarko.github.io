ab3a12836bacca959f8472265bd04b8705c308a2
niinyarko <niinyarko@yahoo.com>
2014-10-11 07:49:22 +0000

    made the img on mobile devices responsive

---
+++ MessageDialogBox.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public  class MessageDialogBox {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Welcome to Java", "Display Message", JOptionPane.INFORMATION_MESSAGE);
+		
+	}
+}
